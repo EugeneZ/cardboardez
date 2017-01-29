5e83eadb6aa689f51bd5728021bbad76a297419f
EugeneZ <eugene.zar@gmail.com>
2017-01-28 20:53:02 -0800

    Refactoring builds so that modules do their own builds

--- gameModuleBuilder.js
+++ gameModuleBuilder.js
@@ -6,7 +6,8 @@
  * - The configuration information. We need this info for ALL games right away, so we will combine the
  *   <package>/configuration.js files from all packages into one. It contains the game name and options.
  * - The client code such as the <PlayArea> component that powers the interface for the game. We only need this when
- *   the player joins the game, so each game needs its own package. We get this from <package>/client/client.js
+ *   the player joins the game, and is already a browserify package, so we just copy it to the public dir. We get this
+ *   from <package>/client/client.js
  *
  *   Finally, we also need a list of all available games so we know what modules are available to fetch.
  */
@@ -14,13 +15,12 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const browserify = require('browserify');
-const babelify = require('babelify');
 const glob = require('glob');
 const jsonfile = require('jsonfile');
 
 const gameModulePrefix = 'cardboardez-game-';
 const destDir = path.join(__dirname, 'public', 'assets', 'scripts', 'games');
-const bConfig = browserify();
+const bundler = browserify();
 const gamelist = [];
 
 glob(path.join(__dirname, 'node_modules', gameModulePrefix + '*'), function (err, files) {
@@ -32,30 +32,22 @@ glob(path.join(__dirname, 'node_modules', gameModulePrefix + '*'), function (err
         const gameName = filename.match(gameModulePrefix + '([a-zA-Z.-]+)')[1];
         gamelist.push(gameName);
 
-        bConfig.require(
+        bundler.require(
             path.join(filename, 'configuration.js'),
             { expose: `${gameModulePrefix}${gameName}-configuration` }
         );
 
         mkdirp(path.join(destDir, gameName), () => {
-            const clientPath = path.join(destDir, gameName, 'client.js');
-            browserify()
-                .require(
-                    path.join(filename, 'client', 'client.js'),
-                    { expose: gameModulePrefix + gameName + '-client' }
-                )
-                .transform(babelify, { global: true })
-                .external('react')
-                .external('react-dom')
-                .external('material-ui')
-                .bundle()
-                .pipe(fs.createWriteStream(clientPath));
+            const sourcePath = path.join(filename, 'dist', 'client.js');
+            const destPath = path.join(destDir, gameName, 'client.js');
+            fs.createReadStream(sourcePath)
+                .pipe(fs.createWriteStream(destPath));
         });
     });
 
     mkdirp(path.join(destDir), () => {
         const configPath = path.join(destDir, 'configurations.js');
-        bConfig.transform(babelify, { global: true }).bundle().pipe(fs.createWriteStream(configPath));
+        bundler.bundle().pipe(fs.createWriteStream(configPath));
     });
 
     jsonfile.writeFileSync(path.join(__dirname, 'gamelist.json'), gamelist);
